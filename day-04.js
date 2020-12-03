d8325f5966eda331074e01128bc9b001c269e911
Vladimir <73591242+SteckoVladimir@users.noreply.github.com>
2020-12-03 23:21:22 +0200

    Update day-04.js

--- day-04.js
+++ day-04.js
@@ -1,7 +1,7 @@
 //  #9: loop statement --while and do..while http://www.codewars.com/kata/57216d4bcdd71175d6000560
 
 function padIt(str, n) {
-  var i = 1;
+  const i = 1;
   while (i <= n) {
     if (i % 2 !== 0) {
       str = "*" + str;
@@ -17,3 +17,34 @@ function padIt(str, n) {
 
 // #10: loop statement --for http://www.codewars.com/kata/5721a78c283129e416000999
 
+function pickIt(arr) {
+  const odd = [],
+    even = [];
+  for (let i = 0; i < arr.length; i++) {
+    if (arr[i] % 2 === 0) {
+      even.push(arr[i]);
+    } else odd.push(arr[i]);
+  }
+  return [odd, even];
+}
+
+
+// #11: loop statement --break,continue http://www.codewars.com/kata/5721c189cdd71194c1000b9b
+
+function grabDoll(dolls){
+  const  bag=[];
+  for (let i = 0; i < dolls.length; i++){
+    
+    if (dolls[i] = "Hello Kitty" || dolls[i] === "Barbie doll" || dolls[i] === "Mickey Mouse" || dolls[i] === "Snow white") {       
+      continue;
+      
+    } else if (i === "Hello Kitty" || i === "Barbie doll" || i === "Mickey Mouse" || i === "Snow white") {
+      bag.push(dolls[i]);
+    } else (dolls.length = 3){
+      break;
+    }
+    
+  }
+  
+  return bag;
+}
