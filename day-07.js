9b664d97628aef33abb5755dba9be6920d00b84d
Vladimir <73591242+SteckoVladimir@users.noreply.github.com>
2020-12-05 18:39:22 +0200

    Create day-07.js

---
+++ day-07.js
@@ -0,0 +1,18 @@
+// #19 Error Throwing - Error Handling #2 https://www.codewars.com/kata/55e7650c8d894146be000095/
+
+function validateMessage(msg) {
+  if (msg.length === 0 || msg.length > 255) {
+    throw new RangeError(`Message contains ${msg.length} characters!`);
+}
+  if (msg !== "some string") {
+    throw new TypeError(`Message should be of type string but was of type ${typeof msg}!`);
+    }
+  
+  if (msg === null) {
+    throw new ReferenceError('Message is null!');
+}
+  if (msg.includes("<") && msg.includes(">")){
+    return false;
+  }
+  return true;
+}
