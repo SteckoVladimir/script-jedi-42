0ee2af844628ea29e6d4da9f616a8a32d26c88e9
Vladimir <73591242+SteckoVladimir@users.noreply.github.com>
2020-12-05 13:36:21 +0200

    Create day-06.js

---
+++ day-06.js
@@ -0,0 +1,16 @@
+// #16 Maximum Multiple https://www.codewars.com/kata/maximum-multiple/train/javascript
+
+function maxMultiple(divisor, bound) {
+  return bound - (bound % divisor);
+}
+
+// #17 Is he gonna survive? https://www.codewars.com/kata/is-he-gonna-survive/train/javascript
+
+function hero(bullets, dragons) {
+  if (bullets / dragons >= 2) {
+    return true;
+  } else return false;
+}
+
+// #18 5 without numbers: https://www.codewars.com/kata/59441520102eaa25260000bf
+
