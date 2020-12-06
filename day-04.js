7bf092e5990b796da349d7ab633ba914b4e62601
Vladimir <73591242+SteckoVladimir@users.noreply.github.com>
2020-12-06 21:58:46 +0200

    Update day-04.js

--- day-04.js
+++ day-04.js
@@ -1,5 +1,4 @@
 //  #9: loop statement --while and do..while http://www.codewars.com/kata/57216d4bcdd71175d6000560
-
 function padIt(str, n) {
   const i = 1;
   while (i <= n) {
@@ -14,9 +13,7 @@ function padIt(str, n) {
   return str;
 }
 
-
 // #10: loop statement --for http://www.codewars.com/kata/5721a78c283129e416000999
-
 function pickIt(arr) {
   const odd = [],
     even = [];
@@ -28,27 +25,22 @@ function pickIt(arr) {
   return [odd, even];
 }
 
-
 // #11: loop statement --break,continue http://www.codewars.com/kata/5721c189cdd71194c1000b9b
-
 function grabDoll(dolls) {
-	const bag = [];
-	for (let i = 0; i < dolls.length; i++) {
-
-		if (dolls[i] !== "Hello Kitty" && dolls[i] !== "Barbie doll")
-			continue; {
-			while (bag.length !== 3) {
-				bag.push(dolls[i]);
-				break;
-			}
-		}
-	}
-
-	return bag;
+  const bag = [];
+  for (let i = 0; i < dolls.length; i++) {
+    if (dolls[i] !== "Hello Kitty" && dolls[i] !== "Barbie doll") continue;
+    {
+      while (bag.length !== 3) {
+        bag.push(dolls[i]);
+        break;
+      }
+    }
+  }
+  return bag;
 }
 
 // #12: loop statement --for..in and for..of	http://www.codewars.com/kata/5722b3f0bd5583cf44001000
-
 function giveMeFive(obj) {
   //coding here
   const five = [];
