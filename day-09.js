5f966a1ee4716a0c1ce02b073e1fd469a9d7e23f
Vladimir <73591242+SteckoVladimir@users.noreply.github.com>
2020-12-08 23:25:26 +0200

    Update day-09.js

--- day-09.js
+++ day-09.js
@@ -1,6 +1,19 @@
 // #16: Methods of String object--slice(), substring() and substr() http://www.codewars.com/kata/57274562c8dcebe77e001012
 
-will be soon
+function cutIt(arr) {
+  let minLengthValue = arr[0].length;
+  const totalArr = [];
+  for (let i = 1; i < arr.length; i++) {
+    if (arr[i].length < minLengthValue) {
+      minLengthValue = arr[i].length;
+    }
+  }
+  for (let i = 0; i < arr.length; i++) {
+    totalArr.push(arr[i].slice(0, minLengthValue));
+  }
+  return totalArr;
+}
+
 
 // #17: Methods of String object--indexOf(), lastIndexOf() and search() http://www.codewars.com/kata/57277a31e5e51450a4000010
 
