309ca820e08ae642ff3560c9b048a6bbe5b4f82a
Vladimir <73591242+SteckoVladimir@users.noreply.github.com>
2020-12-08 19:14:36 +0200

    Create day-09.js

---
+++ day-09.js
@@ -0,0 +1,6 @@
+// #16: Methods of String object--slice(), substring() and substr() http://www.codewars.com/kata/57274562c8dcebe77e001012
+
+will be soon
+
+// #17: Methods of String object--indexOf(), lastIndexOf() and search() http://www.codewars.com/kata/57277a31e5e51450a4000010
+
