31ed5dd0f8728e50a63d2b9340a263b2e5aeb471
Vladimir <73591242+SteckoVladimir@users.noreply.github.com>
2020-12-10 13:59:08 +0200

    Update day-10.js

--- day-10.js
+++ day-10.js
@@ -14,23 +14,24 @@ function alienLanguage(str) {
 #20: Methods of String object--charAt() charCodeAt() and fromCharCode() http://www.codewars.com/kata/57284d23e81185ae6200162a
 
 function topSecret(str){
+  const strArr = str.split('');
   const totalArr = [];
-  for (let i = 0; i < str.length; i++) {
-    if (str[i].charCodeAt() < 65 && str[i].charCodeAt() > 90 && str[i].charCodeAt() < 97 && str[i].charCodeAt() > 123) totalArr.push(str[i]);
+  for (let i = 0; i < strArr.length; i++) {
+    if (strArr[i].charCodeAt() < 65 || strArr[i].charCodeAt() > 90 || strArr[i].charCodeAt() < 97 || strArr[i].charCodeAt() > 123) totalArr.push(strArr[i]);
     
-    if (str[i].charCodeAt() > 67 && str[i].charCodeAt() <= 90) totalArr.push(String.fromCharCodeAt(str[i].charCodeAt() - 3));
+    if (strArr[i].charCodeAt() > 67 && strArr[i].charCodeAt() <= 90) totalArr.push(String.fromCharCode(strArr[i].charCodeAt() - 3));
     // A = 65 => X = 88  ; B = 66 => Y = 89  ;  C = 67 => Z = 90.
-    if (str[i] = 'A') totalArr.push('X');
-    if (str[i] = 'B') totalArr.push('Y');
-    if (str[i] = 'C') totalArr.push('Z');
+    if (strArr[i] = 'A') totalArr.push('X');
+    if (strArr[i] = 'B') totalArr.push('Y');
+    if (strArr[i] = 'C') totalArr.push('Z');
     
-    if (str[i].charCodeAt() > 99 && str[i].charCodeAt() <= 123) totalArr.push(String.fromCharCodeAt(str[i].charCodeAt() - 3));
+    if (strArr[i].charCodeAt() > 99 && strArr[i].charCodeAt() <= 123) totalArr.push(String.fromCharCode(strArr[i].charCodeAt() - 3));
     // a = 97 => x = 121  ; b = 98 => y = 122  ;  c = 99 => z = 123.
-    if (str[i] = 'a') totalArr.push('x');
-    if (str[i] = 'b') totalArr.push('y');
-    if (str[i] = 'c') totalArr.push('z');
+    if (strArr[i] = 'a') totalArr.push('x');
+    if (strArr[i] = 'b') totalArr.push('y');
+    if (strArr[i] = 'c') totalArr.push('z');
   }
-return totalArr;
+return totalArr.join('');
 }
 
 //question1: The top secret file number is...
