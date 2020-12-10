e1cbc6aea67a8bf0ee3c36f78d615f2ff7989167
Vladimir <73591242+SteckoVladimir@users.noreply.github.com>
2020-12-10 13:09:34 +0200

    Update day-10.js

--- day-10.js
+++ day-10.js
@@ -14,10 +14,23 @@ function alienLanguage(str) {
 #20: Methods of String object--charAt() charCodeAt() and fromCharCode() http://www.codewars.com/kata/57284d23e81185ae6200162a
 
 function topSecret(str){
-  return str
-            .split('')
-            .map( char => String.fromCharCode(char.charCodeAt() - 3))
-            .join('');
+  const totalArr = [];
+  for (let i = 0; i < str.length; i++) {
+    if (str[i].charCodeAt() < 65 && str[i].charCodeAt() > 90 && str[i].charCodeAt() < 97 && str[i].charCodeAt() > 123) totalArr.push(str[i]);
+    
+    if (str[i].charCodeAt() > 67 && str[i].charCodeAt() <= 90) totalArr.push(String.fromCharCodeAt(str[i].charCodeAt() - 3));
+    // A = 65 => X = 88  ; B = 66 => Y = 89  ;  C = 67 => Z = 90.
+    if (str[i] = 'A') totalArr.push('X');
+    if (str[i] = 'B') totalArr.push('Y');
+    if (str[i] = 'C') totalArr.push('Z');
+    
+    if (str[i].charCodeAt() > 99 && str[i].charCodeAt() <= 123) totalArr.push(String.fromCharCodeAt(str[i].charCodeAt() - 3));
+    // a = 97 => x = 121  ; b = 98 => y = 122  ;  c = 99 => z = 123.
+    if (str[i] = 'a') totalArr.push('x');
+    if (str[i] = 'b') totalArr.push('y');
+    if (str[i] = 'c') totalArr.push('z');
+  }
+return totalArr;
 }
 
 //question1: The top secret file number is...
