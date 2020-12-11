638223cedc1446486b1b14f133a6662edc36faa6
Vladimir <73591242+SteckoVladimir@users.noreply.github.com>
2020-12-11 13:14:15 +0200

    Update day-11-TDD-Bin-SET.js

--- day-11-TDD-Bin-SET.js
+++ day-11-TDD-Bin-SET.js
@@ -70,3 +70,119 @@ describe('`add()` appends a new element to the end of a Set object.', function()
     assert.equal(set.has(+0), true);
   });
 });
+
+// set.delete() http://tddbin.com/#?kata=es6/language/set/delete
+
+// 64: Set - delete
+// To do: make all tests pass, leave the assert lines unchanged!
+// Follow the hints of the failure messages!
+
+describe('`set.delete()` deletes an element from a set', function(){
+  let set;
+  beforeEach(() => set = new Set());
+  describe('use `delete(<value>)` to delete an element', function() {
+    beforeEach(function() {
+      set.add('one').add('two').add('three');
+    });
+    it('`delete()` returns `true` when the element was found', function() {
+      const returns = set.delete('one','two','three');
+      assert.strictEqual(returns, true);
+    });
+    it('and the size decreases', function() {
+      
+      assert.equal(set.size, 3);
+    });
+  });
+  describe('if nothing was deleted (no element with the given value was found)', function() {
+    it('returns `false`', function() {
+      set.add('one');
+      
+      const returns = set.delete('five');
+      assert.equal(returns, false);
+    });
+  });
+  describe('`undefined` is a valid value in a set', function() {
+    it('deleting it, when it is not in the set, returns `false` too', function() {
+      
+      assert.equal(set.delete(), false);
+    });
+    it('`delete()` removes it, when its in the set', function() {
+      set.add(5);
+      assert.equal(set.delete(5), true);
+    });
+  });
+  describe('the value does NOT get casted', function() {
+    it('number 1 is different to string "1"', function() {
+      set.add(1);
+      set.add(1);
+      assert.equal(set.delete('1'), false);
+    });
+  });
+});
+
+// Set API http://tddbin.com/#?kata=es6/language/set/api
+
+// 65: Set - API overview
+// To do: make all tests pass, leave the assert lines unchanged!
+// Follow the hints of the failure messages!
+
+describe('`Set` API overview', function(){
+  const api = ['size', 'add', 'clear', 'delete', 'entries', 'forEach', 'has', 'keys', 'values'];
+  let set;
+  beforeEach(function() {
+    set = new Set(api);
+  });
+  it('a Set can be created from an array', function() {
+    let set = new Set(api);
+    assert.deepEqual(Array.from(set), api);
+  });
+  it('`size` is the number of values', function() {
+    const theSize = set.size;
+    assert.equal(theSize, api.length);
+  });
+  it('`add()` appends the given value', function() {
+    // Hint: To make the content of `api` and `set` consistent you can add the
+    // `Symbol.iterator` to the `set`. Strongly speaking it is missing in the API.
+    set.add('nine');
+    assert.equal(set.size, api.length + 1);
+  });
+  it('`clear()` removes all elements', function() {
+    set.clear();
+    assert.equal(set.size, 0);
+  });
+  it('`delete()` removes the given value', function() {
+    
+    set.delete('entries');
+    assert.equal(set.size, api.length - 1);
+  });
+  it('`entries()` returns an iterator for all values', function() {
+    const expectedEntries = api.map(entry => [entry, entry]); 
+    const actualEntries = set.entries();
+    assert.deepEqual([...actualEntries], expectedEntries);
+  });
+  it('`forEach()` calls a callback for each value', function() {   ///!!!!!!!!!!
+    let values = [];
+    set.map(value => { values.push(value); });
+    assert.deepEqual(values, api);
+  });
+  it('`has()` returns true if the given value is in the set', function() {
+    const propertyName = '';
+    set.add('');
+    assert.equal(set.has(propertyName), true);
+  });
+  describe('returns an iterator that contains all values', function() {
+    // In order to be alike to `Map`, `keys()` and `values()` are essentially the same thing for a `Set`.
+    it('`keys()`', function() {
+      const allKeys = api;
+      assert.deepEqual([...allKeys], api);
+    });
+    it('`values()`', function() {
+      const allValues = api;
+      assert.deepEqual([...allValues], api);
+    });
+    it('`[Symbol.iterator]()`', function() {
+      const iteratorKey = '???';
+      assert.deepEqual([...set[iteratorKey]()], api);
+    });
+  });
+});
