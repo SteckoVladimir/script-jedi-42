694d8e868584c15abdc55b8bb1dc90178075cc2f
Vladimir <73591242+SteckoVladimir@users.noreply.github.com>
2020-12-12 15:11:03 +0200

    Update day-12-SETS.js

--- day-12-SETS.js
+++ day-12-SETS.js
@@ -1,19 +1,17 @@
 // Playing with Sets : Equal or Not ?	https://www.codewars.com/kata/playing-with-sets-equal-or-not/train/javascript
 
-function areEqual(s1, s2){
-
-  for (let value of s1) {
-    if (s1.has(value) === s2.has(value)) {
-      return true;
-    } return false;
+function areEqual(s1, s2) {
+  if (s1.size === s2.size) {
+    for (let value of s1) {
+      if (!s2.has(value)) {
+        return false;
+      }
+    }
+    return true;
   }
+  return false;
 }
 
-function notEqual(s1, s2){
-
-  for (let value of s1) {
-    if (s1.has(value) !== s2.has(value)) {
-      return true;
-    } 
-  } return false;
+function notEqual(s1, s2) {
+  return !areEqual(s1, s2);
 }
