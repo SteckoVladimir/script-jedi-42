56073fc89219701d00fc7789a27eb6f1480d2d8a
Vladimir <73591242+SteckoVladimir@users.noreply.github.com>
2020-12-14 21:34:21 +0200

    Update day-14-Arrow-function-Spread-and-deconstruction.js

--- day-14-Arrow-function-Spread-and-deconstruction.js
+++ day-14-Arrow-function-Spread-and-deconstruction.js
@@ -1,2 +1,11 @@
 // #22: Unlock new skills--Arrow function,spread operator and deconstruction http://www.codewars.com/kata/572ab0cfa3af384df7000ff8
 
+function shuffleIt(arr, ...arrIndex){
+  for (const [a, b] of arrIndex) {
+  [arr[a],arr[b]] = [arr[b],arr[a]];
+}
+  return arr;
+}
+
+//  #23: methods of arrayObject---push(), pop(), shift() and unshift() http://www.codewars.com/kata/572af273a3af3836660014a1
+
