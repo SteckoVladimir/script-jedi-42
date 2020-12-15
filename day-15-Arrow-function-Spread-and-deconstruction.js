d87c1cc3e0f2fb0e8f880b9bd5d4b2eae699a9bb
Vladimir <73591242+SteckoVladimir@users.noreply.github.com>
2020-12-15 13:33:26 +0200

    Update day-15-Arrow-function-Spread-and-deconstruction.js

--- day-15-Arrow-function-Spread-and-deconstruction.js
+++ day-15-Arrow-function-Spread-and-deconstruction.js
@@ -9,3 +9,21 @@ function shuffleIt(arr, ...arrIndex){
 
 //  #23: methods of arrayObject---push(), pop(), shift() and unshift() http://www.codewars.com/kata/572af273a3af3836660014a1
 
+function infiniteLoop(arr,d,n){
+  //  arr - массив  ,  d - 'sring'  ,  n - number;
+  const shifted =  [...arr];
+  const myArrSize = shifted.join().split(',').length;
+  console.log(myArrSize);
+   for (let block of shifted)
+     if(d === "left") {
+       for (let le of block) {
+         if (le > n) le = le - n;
+         if ( le === n) le = myArrSize;
+         if ( le < n) le = le + myArrSize - n;
+
+         
+         console.log(le);
+       }
+     }  
+  console.log(le);
+}
