855c6ec35016acbb797283373baabfc7c13b4233
Vladimir <73591242+SteckoVladimir@users.noreply.github.com>
2020-12-16 17:25:26 +0200

    Update day-16-Methods-of-Array-object.js

--- day-16-Methods-of-Array-object.js
+++ day-16-Methods-of-Array-object.js
@@ -1,11 +1,17 @@
 // #25: methods of arrayObject---reverse() and sort() http://www.codewars.com/kata/572df796914b5ba27c000c90
 
-function sortIt(arr){
- const workArr = [...arr];
- const totalArr = [];
-  workArr.sort((a,b) =>{
-    if (a > b) return b - a;
-    if (a !== b) return 1;
+function sortIt(arr) {
+  const workArr = [...arr];
+  workArr.sort((a, b) => {
+    let elemACount = arr.slice().filter((x) => x === a).length;
+    let elemBCount = arr.slice().filter((y) => y === b).length;
+    if (elemACount === elemBCount) return b - a;
+    if (elemACount > elemBCount) return 1;
+    else return -1;
   });
-return workArr;
+  return workArr;
 }
+
+// #26: methods of arrayObject---map() http://www.codewars.com/kata/572fdeb4380bb703fc00002c
+
+
