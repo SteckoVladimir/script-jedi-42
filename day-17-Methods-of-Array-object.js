9f2c7248f8bb0a9797dcba44365667bba2a6e396
Vladimir <73591242+SteckoVladimir@users.noreply.github.com>
2020-12-18 14:17:54 +0200

    Update day-17-Methods-of-Array-object.js

--- day-17-Methods-of-Array-object.js
+++ day-17-Methods-of-Array-object.js
@@ -6,3 +6,31 @@ function bigToSmall(arr){
   const result = workArrSort.join('>');
   return result;
 }
+
+//  #30: methods of arrayObject---reduce() and reduceRight() http://www.codewars.com/kata/573156709a231dcec9000ee8
+
+function tailAndHead(arr){
+  const workArr = arr;
+  const tailArr = [];
+  const headArr = [];
+ 
+  for (let i = 0; i < workArr.length-1; i++) {
+    tailArr.push(+String(workArr[i]).slice(-1));   
+  }
+    for (let i = 1; i < workArr.length; i++) {
+    headArr.push(+String(workArr[i]).slice(0,1));   
+  }
+  function sum (a , b) {
+    for (let i = 0; i < a.length; i++) {
+       a[i] + b[i];
+    }
+  }
+   const totalArr = sum(headArr , tailArr);
+   console.log(totalArr);
+
+}
+
+// #31: methods of arrayObject---isArray() indexOf() and toString() http://www.codewars.com/kata/5732b0351eb838d03300101d
+
+ 
+
