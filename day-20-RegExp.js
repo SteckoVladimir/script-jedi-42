12693dfa066bf0b6336cd15b5925acbcc62e8ba2
Vladimir <73591242+SteckoVladimir@users.noreply.github.com>
2020-12-22 19:47:36 +0200

    Update day-20-RegExp.js

--- day-20-RegExp.js
+++ day-20-RegExp.js
@@ -8,6 +8,17 @@ function countAnimals(animals, count) {
   return workResult;
 }
 
+// #38: Regular Expression--""^"",""$"", ""."" and test() http://www.codewars.com/kata/573975d3ac3eec695b0013e0
+
+function findSimilarity(str,word){
+const re = new RegExp('^' + word[0] + '.{' + (word.length - 2) +'}'
+  + word.slice(-1) + '$');
+  return str
+    .split(' ')
+    .filter(w => re.test(w))
+    .join(' ');
+}
+
 // #39: Regular Expression--""?"", ""*"", ""+"" and ""{}"" https://www.codewars.com/kata/573bca07dffc1aa693000139
 
 var regex=/^-?9\d*0000$/;
