7b7b96b11f0878fb6b4be4406b573da5d8ec1265
Vladimir <73591242+SteckoVladimir@users.noreply.github.com>
2020-12-22 22:35:14 +0200

    Update day-22-TTD-Bin-Arrow-Functions.js

--- day-22-TTD-Bin-Arrow-Functions.js
+++ day-22-TTD-Bin-Arrow-Functions.js
@@ -29,3 +29,35 @@ describe('Arrow functions', function() {
 
 // Function binding http://tddbin.com/#?kata=es6/language/arrow-functions/binding
 
+// 6: arrow functions - binding
+// To do: make all tests pass, leave the asserts unchanged!
+// Follow the hints of the failure messages!
+
+class LexicallyBound {
+  getFunction() {
+    return() => this;
+}
+  getArgumentsFunction() {
+    return() => arguments;
+}
+}
+
+describe('Arrow functions have lexical `this`, no dynamic `this`', () => {
+  it('bound at definition time, use `=>`', function() {
+    var bound = new LexicallyBound();
+    var fn = bound.getFunction();
+    assert.strictEqual(fn(), bound);
+  });
+  it('can NOT bind a different context', function() {
+    var bound = new LexicallyBound();
+    var fn = bound.getFunction();
+    var anotherObj = {};
+    var expected = bound;
+    assert.strictEqual(fn.call(anotherObj), expected);
+  });
+  it('`arguments` does NOT work inside arrow functions', function() {
+    var bound = new LexicallyBound();
+    var fn = bound.getArgumentsFunction();
+    assert.equal(fn(1, 2).length, 0);
+  });
+});
