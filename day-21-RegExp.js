561235a60e25079afc0aad608a8563d8e0c72601
Vladimir <73591242+SteckoVladimir@users.noreply.github.com>
2020-12-22 21:55:48 +0200

    Create day-21-RegExp.js

---
+++ day-21-RegExp.js
@@ -0,0 +1,14 @@
+// #40: Regular Expression--""|"", ""[]"" and ""()"" http://www.codewars.com/kata/573d11c48b97c0ad970002d4
+
+var regex = /(http:\/\/|https:\/\/)[a-z0-9.]+\.(com|net)/gi;
+
+// #41: Regular Expression--"""" http://www.codewars.com/kata/573e6831e3201f6a9b000971
+
+var regex=/\b(\w)(\w)?(\w)?\w?\3\2\1\b/g
+
+// #42: Regular Expression--(?:), (?=) and (?!) http://www.codewars.com/kata/573fb9223f9793e485000453
+
+const regex = /\d(?=(\d{3})+$)/g;
+function addCommas(money, reg) {
+  return money.replace(reg, (x) => x + ",");
+}
