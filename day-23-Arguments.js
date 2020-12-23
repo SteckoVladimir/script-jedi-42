0b91c81b5e3e454b37ea60b38fcf749792010798
Vladimir <73591242+SteckoVladimir@users.noreply.github.com>
2020-12-23 17:08:02 +0200

    Update day-23-Arguments.js

--- day-23-Arguments.js
+++ day-23-Arguments.js
@@ -3,6 +3,14 @@
 function spread(func, args) {
   return func(...args);
 }
+// or (I practice
+const spread = (func, args) => func(...args);
 
 // For the sake of argument http://www.codewars.com/kata/for-the-sake-of-argument
 
+function numbers(...itemsToCheck) {
+  for (let i = 0; i < itemsToCheck.length; i++) {
+    if (typeof itemsToCheck[i] !== "number") return false;
+  }
+  return true;
+}
