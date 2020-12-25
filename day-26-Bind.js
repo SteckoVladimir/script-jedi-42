f3963f848fe2d8585e905682db5963e0bebbf623
Vladimir <73591242+SteckoVladimir@users.noreply.github.com>
2020-12-25 16:34:42 +0200

    Update day-26-Bind.js

--- day-26-Bind.js
+++ day-26-Bind.js
@@ -1,6 +1,9 @@
 //  Power of bind http://www.codewars.com/kata/power-bind
 
-OMG...
+// OMG...
 
 // Sort with Arrow Functions https://www.codewars.com/kata/sort-with-arrow-functions
 
+const OrderPeople = function (people) {
+  return people.sort((a, b) => a.age - b.age);
+};
