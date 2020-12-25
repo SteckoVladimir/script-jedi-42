997d57ed62e3d493f6d32f4769b79f66e66cf163
Vladimir <73591242+SteckoVladimir@users.noreply.github.com>
2020-12-25 18:09:05 +0200

    Create day-27-Closures.js

---
+++ day-27-Closures.js
@@ -0,0 +1,9 @@
+//  Understanding closures - the  basics https://www.codewars.com/kata/56b71b1dbd06e6d88100092a
+
+function buildFun(n) {
+  const res = [];
+  for (let i = 0; i < n; i++) res.push(() => i);
+  return res;
+}
+
+//  Shifty closures https://www.codewars.com/kata/shifty-closures/train/javascript
