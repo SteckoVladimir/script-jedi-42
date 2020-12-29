53eea48822afed974194210f72e9d3fe7356dabb
Vladimir <73591242+SteckoVladimir@users.noreply.github.com>
2020-12-29 14:22:29 +0200

    Update day-29-Understanding-new.js

--- day-29-Understanding-new.js
+++ day-29-Understanding-new.js
@@ -8,3 +8,10 @@ myObj.constructor();
 // Replicate new http://www.codewars.com/kata/replicate-new
 
 const nouveau = (Constructor, ...args) => Reflect.construct(Constructor, args);
+
+// понимаю что надо так НО НЕ ПОНИМАЮ ЭТО с 3-й строчки !!! 
+function nouveau (Constructor, ...args) {
+  const instance = Object.create(Constructor.prototype);
+  const result = Constructor.apply(instance, args);
+  return result === Object(result) ? result : instance;
+}
