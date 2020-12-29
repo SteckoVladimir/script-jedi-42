d95dc0695123d26fe2ace9b7e3ce2a9fb3cf8ff2
Vladimir <73591242+SteckoVladimir@users.noreply.github.com>
2020-12-29 11:12:16 +0200

    Update day-26-Bind.js

--- day-26-Bind.js
+++ day-26-Bind.js
@@ -1,6 +1,12 @@
 //  Power of bind http://www.codewars.com/kata/power-bind
 
-// OMG...
+Function.prototype.bind = function (ctx) {
+  const func = this;
+  return function (...args) {
+    const correctCtx = this === global ? ctx : this;
+    return func.apply(correctCtx, args);
+  }
+};
 
 // Sort with Arrow Functions https://www.codewars.com/kata/sort-with-arrow-functions
 
