5897a4ffeaf2610f842ee4cb5f9b4667bb387787
Vladimir <73591242+SteckoVladimir@users.noreply.github.com>
2020-12-29 17:49:39 +0200

    Update day-30-Duck-Typing-and-Inheritance.js

--- day-30-Duck-Typing-and-Inheritance.js
+++ day-30-Duck-Typing-and-Inheritance.js
@@ -1,2 +1,6 @@
 // SantaClausable Interface http://www.codewars.com/kata/santaclausable-interface
 
+const isSantaClausable = obj =>'function' === typeof (obj.sayHoHoHo && obj.distributeGifts && obj.goDownTheChimney);
+
+// Cylon Evolution http://www.codewars.com/kata/cylon-evolution
+
