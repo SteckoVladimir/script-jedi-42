048581ced883a4cdf8f5e2a747941df867a86b28
Vladimir <73591242+SteckoVladimir@users.noreply.github.com>
2021-01-02 16:47:21 +0200

    Update day-30-Duck-Typing-and-Inheritance.js

--- day-30-Duck-Typing-and-Inheritance.js
+++ day-30-Duck-Typing-and-Inheritance.js
@@ -22,3 +22,18 @@ class HumanSkin extends Cylon {
 }
 
 //Simple Web Framework #1: Create a basic router https://www.codewars.com/kata/588a00ad70720f2cd9000005/train/javascript
+
+class Router {
+  constructor() {
+    this.routes = new Map();
+  }
+  bind(url, method, action) {
+    this.routes.set(url + method, action);
+  }
+  runRequest(url, method) {
+    if (!this.routes.has(url + method)) {
+      return "Error 404: Not Found";
+    }
+    return this.routes.get(url + method)();
+  }
+}
