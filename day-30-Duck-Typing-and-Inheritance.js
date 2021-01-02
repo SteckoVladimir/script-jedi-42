32fe82d045258caa385505ad4d08433f60659266
Vladimir <73591242+SteckoVladimir@users.noreply.github.com>
2021-01-02 16:15:22 +0200

    Update day-30-Duck-Typing-and-Inheritance.js

--- day-30-Duck-Typing-and-Inheritance.js
+++ day-30-Duck-Typing-and-Inheritance.js
@@ -4,3 +4,21 @@ const isSantaClausable = obj =>'function' === typeof (obj.sayHoHoHo && obj.distr
 
 // Cylon Evolution http://www.codewars.com/kata/cylon-evolution
 
+class Cylon {
+  constructor(model) {
+    this.model = model;
+  }
+  attack() {
+    return "Destroy all humans!";
+  }
+}
+class HumanSkin extends Cylon {
+  constructor(model) {
+    super(model);
+  }
+  infiltrate() {
+    return "Infiltrate the colonies";
+  }
+}
+
+//Simple Web Framework #1: Create a basic router https://www.codewars.com/kata/588a00ad70720f2cd9000005/train/javascript
