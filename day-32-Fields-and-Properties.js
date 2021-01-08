6ab1098d07e2935f495ab2184834424512081d2b
Vladimir <73591242+SteckoVladimir@users.noreply.github.com>
2021-01-08 18:09:46 +0200

    Update day-32-Fields-and-Properties.js

--- day-32-Fields-and-Properties.js
+++ day-32-Fields-and-Properties.js
@@ -1,2 +1,25 @@
 //   "this"" is a problem - http://www.codewars.com/kata/this-is-a-problem
 
+function NameMe(first, last) {
+  this.firstName = first;
+  this.lastName = last;
+  this.name = first + " " + last;
+}
+
+// ""this"" is an other problem - https://www.codewars.com/kata/this-is-an-other-problem
+
+class NamedOne {
+  constructor(first, last){
+    this.firstName = first;
+    this.lastName = last;
+  }
+   get fullName () {
+     return this.fullName = this.firstName + ' ' + this.lastName;
+   }
+  set fullName(value) {
+    const names = value.split(' ')
+    if (names.length === 2) {
+      this.firstName = names[0];
+      this.lastName = names[1];
+    }
+  }
