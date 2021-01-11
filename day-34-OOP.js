c74c325ebf5883989a9df860d8c6ebeb7589185c
Vladimir <73591242+SteckoVladimir@users.noreply.github.com>
2021-01-11 13:32:22 +0200

    Create day-34-OOP.js

---
+++ day-34-OOP.js
@@ -0,0 +1,21 @@
+//   Adam and Eve - https://www.codewars.com/kata/basic-subclasses-adam-and-eve/
+
+class God{
+  static create(){
+    return [new Man, new Woman()]
+  }
+}
+class Human {}
+class Man extends Human {}
+class Woman extends Human {}
+
+// Labrador - https://www.codewars.com/kata/56ff9b53140fcca90b000530/
+
+class Labrador extends Dog {
+  constructor(name, age, gender, master) {
+    super(name, age, gender, "Labrador", "Large", master, true);
+  }
+}
+
+//  Classy Classes - https://www.codewars.com/kata/55a144eff5124e546400005a
+
