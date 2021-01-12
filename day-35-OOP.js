2ac96b88dd55aa08bcc72a7302070f0f6c078607
Vladimir <73591242+SteckoVladimir@users.noreply.github.com>
2021-01-12 11:48:03 +0200

    Update day-35-OOP.js

--- day-35-OOP.js
+++ day-35-OOP.js
@@ -1,2 +1,17 @@
 //  Object Prototypes https://www.codewars.com/kata/js-prototypes-module-number-1-object-prototypes
 
+widget.__proto__.description = "TODO: GIVE ME A DESCRIPTION";
+gadget.__proto__.description = "TODO: GIVE ME A DESCRIPTION";
+thingamabob.__proto__.description = "TODO: GIVE ME A DESCRIPTION";
+
+//  Class-Like Objects https://www.codewars.com/kata/javascript-class-like-objects
+
+class Animal {
+  constructor(name, type) {
+    this.name = name;
+    this.type = type;
+  }
+  toString() {
+    return this.name + " is a " + this.type;
+  }
+}
