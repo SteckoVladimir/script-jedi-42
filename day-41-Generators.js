fc45f6b29aed16dc99e8f10710949dfdc97c3ac2
Vladimir <73591242+SteckoVladimir@users.noreply.github.com>
2021-01-16 18:48:28 +0200

    Update day-41-Generators.js

--- day-41-Generators.js
+++ day-41-Generators.js
@@ -1,21 +1,11 @@
 //  Generators #1 https://www.codewars.com/kata/basics-generators-number-1/javascript
 
 function* generator(n) {
- let counter = 0;
-   for (let i =1; i < Number.MAX_SAFE_INTEGER; i++) {
-   if (n !== undefined) yield counter = n;
-   if (n === undefined) yield counter = i;
- }
- return counter;
-}
-
-function* generator(n) {
-  let index = 1;
-  if (n !== undefined) {
-    yield index = n;
-  }
-  if (n === undefined) {
-    while(true)
-    yield index++;
+  for (let i = 1; i < Number.MAX_SAFE_INTEGER; i++) {
+    let n = yield i;
+    if (n !== undefined) {
+      i = n;
+      n = yield n;
+    }
   }
 }
