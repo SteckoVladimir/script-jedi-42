f1ad440e7e95e27150c0749942cd15a4a126b444
Vladimir <73591242+SteckoVladimir@users.noreply.github.com>
2021-01-19 11:49:01 +0200

    Update day-42-Two-Sum-Problem.js

--- day-42-Two-Sum-Problem.js
+++ day-42-Two-Sum-Problem.js
@@ -1,2 +1,13 @@
 //  https://leetcode.com/problems/two-sum/
 
+const twoSum = function(nums, target) {
+  let result = [];
+    for (let i = 0; i < nums.length; i++) {
+      for (let iter = 1; iter < nums.length; iter++) {
+        if ( nums[i] + nums[iter] === target) {
+          result.push(i,iter)
+        }
+      }
+    }
+  return result;
+};
