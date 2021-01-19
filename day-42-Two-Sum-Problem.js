0a05910963de8fcf783485a6356c1e57dbd68f1d
Vladimir <73591242+SteckoVladimir@users.noreply.github.com>
2021-01-19 12:26:45 +0200

    Update day-42-Two-Sum-Problem.js

--- day-42-Two-Sum-Problem.js
+++ day-42-Two-Sum-Problem.js
@@ -2,12 +2,16 @@
 
 const twoSum = function(nums, target) {
   let result = [];
-    for (let i = 0; i < nums.length; i++) {
-      for (let iter = 1; iter < nums.length; iter++) {
+    for (let i = 0; i < nums.length-1; i++) {
+      for (let iter = i+ 1; iter < nums.length; iter++) {
         if ( nums[i] + nums[iter] === target) {
-          result.push(i,iter)
+          result.push(i,iter);
         }
       }
     }
   return result;
 };
+
+// and 
+
+
